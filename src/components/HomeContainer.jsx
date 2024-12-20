c32adfb65320553c1582814595747749fdb7b4cb
GarzaF1109 <fernandogarzamedina3@gmail.com>
2024-12-20 23:31:47 +0000

    Se empezó el apartado de Create Container para gregar platillos nuevos desde cuneta administrador

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Delivery from '../img/delivery.png'
 import HeroBg from '../img/heroBg.png'
+import { heroData } from '../utils/data'
 
 
 
@@ -19,10 +20,21 @@ const HomeContainer = () => {
     <p className='text-base text-textColor text-center md:text-left md:w-[80%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat eum quam cum? Dolores repudiandae in porro. Quisquam suscipit eos nemo ipsa voluptatum ipsam velit placeat, eveniet, illo, distinctio hic voluptates?</p>
     <button type='button' className='bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100'>Order Now</button>
     </div>
-    <div className='py-2 flex-1 flex items-center'>
+    <div className='py-2 flex-1 flex items-center relative'>
         
-            <img src={HeroBg} className='ml-auto h-650' alt='hero-bg'/>
-            <div className='w-full h-full absolute flex items-center justify-center'></div>
+            <img src={HeroBg} className='ml-auto h-420 w-full lg:w-auto lg:h-650' alt='hero-bg'/>
+            <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32 py-4 gap-4 flex-wrap'>
+              {heroData && heroData.map(n =>(
+                              <div key={n.id} className='lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg'>
+                              <img src={n.imageSrc} className='w-20 lg:w-40 -mt-10 lg:-mt-20' alt="I1" />
+                              <p className='text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4'>{n.name}</p>
+                              <p className='text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3'>{n.decp}</p>
+              
+                              <p className='text-sm font-semibold text-headingColor'><span className='text-xs text-red-600'>$</span>{n.price}</p>
+                            </div>
+              ))}
+
+            </div>
         
     </div>
   </section>
