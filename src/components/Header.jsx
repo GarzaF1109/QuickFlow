97f0f617949b5a2807546b79b3810ba5e9011d94
GarzaF1109 <fernandogarzamedina3@gmail.com>
2025-01-09 21:24:29 +0000

    SE TERMINÓ EL FEATURE DE KITCHENVIEW

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -90,6 +90,9 @@ const Header = () => {
                                                 <Link to={"/editItem"}>
                                                     <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' onClick={() => setIsMenu(false)}>Edit Item <FaPencilAlt /></p>
                                                 </Link>
+                                                <Link to={"/ticketsView"}>
+                                                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' onClick={() => setIsMenu(false)}>Ver Pedidos <BiSolidDish /></p>
+                                                </Link>
                                             </>
                                         )  
                                     }
@@ -149,9 +152,21 @@ const Header = () => {
                                             <Link to={"/editItem"}>
                                                 <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' onClick={() => setIsMenu(false)}>Edit Item <FaPencilAlt /></p>
                                             </Link>
+                                            <Link to={"/ticketsView"}>
+                                                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' onClick={() => setIsMenu(false)}>Ver Pedidos <BiSolidDish /></p>
+                                            </Link>
                                         </>
                                     )
                                 }
+                                                                    {user && user.email === "fernandogarza801@gmail.com" && (
+                                            <>
+                                                <Link to={"/ticketsView"}>
+                                                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' onClick={() => setIsMenu(false)}>Ver Pedidos <BiSolidDish /></p>
+                                                </Link>
+                                                
+                                            </>
+                                        )}
+
                                 <ul className='flex flex-col'>
                                     <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={() => setIsMenu(false)}>Menu</li>
                                     <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2' onClick={() => setIsMenu(false)}>About Us</li>
