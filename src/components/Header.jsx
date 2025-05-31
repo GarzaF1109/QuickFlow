c8dd015923806d9ca764778de90fc2a0902e47ff
GarzaF1109 <fernandogarzamedina3@gmail.com>
2025-05-31 11:19:19 -0600

    Translation of thw page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -145,7 +145,7 @@ const Header = () => {
                                                 <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' onClick={() => setIsMenu(false)}>Edit Item <FaPencilAlt /></p>
                                             </Link>
                                             <Link to={"/ticketsView"}>
-                                                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' onClick={() => setIsMenu(false)}>Ver Pedidos <BiSolidDish /></p>
+                                                    <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base' onClick={() => setIsMenu(false)}>Orders <BiSolidDish /></p>
                                             </Link>
                                         </>
                                     )
